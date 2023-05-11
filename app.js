f6eb719892c6c9777beaff82cba887a380f903eb
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-11 12:36:03 -0400

    add compose page with post method

--- app.js
+++ app.js
@@ -20,19 +20,19 @@ app.get("/", function(req, res) {
 
 app.get("/about", function(req, res) {
   res.render("about", {aboutPage: aboutContent});
-})
+});
 
 app.get("/contact", function(req, res) {
   res.render("contact", {contactPage: contactContent});
-})
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
-
-
-
-
-
-
+app.post("/compose", function(req, res) {
+  console.log(req.body.postTitle);
+});
 
 
 app.listen(3000, function() {
